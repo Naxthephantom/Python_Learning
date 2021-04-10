860ecaf5aae5b931652f5b72819f856e61c5a605
Naxthephantom <80404140+Naxthephantom@users.noreply.github.com>
2021-04-10 11:09:05 +0100

    Update Web_scraping.py

--- Web_scraping.py
+++ Web_scraping.py
@@ -1,20 +1,16 @@
-import pandas as pd
+import requests
 from bs4 import Beautifulsoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import options
-options = options()
-options.binary_location = ("C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe")
-driver_path = ("C:\Users\Home\Downloads\chromedriver\chromedriver.exe")
-driver = webdriver.chrome(options = options, executable_path = driver-path)
-pl_team =[] #list to store name of club
-games_lost =[] #list to store number of games played
-games_won =[] #list to store number of games won
-games_drawn =[] #list to store number of games drawn
-team_points =[] # list to store total points
+
 url = "https://www.skysports.com/premier-league-table"
-driver.get(url)
-content = driver.page_source
-soup = Bautifulsoup(content)
+
+r = request.get(url)
+Club=[] #list to store name of club
+Position=[] #list to store position of the club
+L=[] #list to store number of games played
+W=[] #list to store number of games won
+D=[] #list to store number of games drawn
+GD=[] #list to store goal diffrence
+Pts=[] # list to store total points
 pl_table = soup.find("table", class_ = "standing-table__table callfn")
 for team in pl_table.find_all("tbody"):
     rows = team.find_all("tr")
